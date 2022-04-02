6c387b54687215bec79b74327080ad8c54ab7400
EugeneSA03 <EugeneSA03@gmail.com>
2022-04-03 02:04:41 +0400

    Время видел?
    Сколько можно еще это вот все?

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -4,62 +4,64 @@ using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour
 {
-    private class Enemy {
-        public GameObject prefab = null;
-        public float speed = 0;
-        
+    class Enemy {
+        public GameObject body;
+        public float speed;
+        public Vector3 rotation;
     }
 
-    public GameObject prefab = null;
-    public int maxEnemies = 1;
-    public float maxEnemyScale = 3;
-    public uint maxEnemySpeed = 3;
-    public uint minEnemySpeed = 3;
+    public GameObject[] enemyMeshs = null;
 
+    public int enemyScaleMax = 200;
+
+    public int enemySpeedMin = 20;
+    public int enemySpeedMax = 90;
+
+    public uint numOfEnemies = 300;
+
+    private List<Enemy> enemies;
     private GameObject spawner = null;
-    private List<Enemy> enemies = null;
-    private Enemy enemyTemp = null;
     private System.Random random = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        enemies = new List<Enemy>();
         spawner = this.gameObject;
-        enemies = new List<Enemy> ();
-        random = new System.Random ();
+        random = new System.Random();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (enemies.Count < numOfEnemies) {
+            Enemy et = new Enemy();
+            et.body = Instantiate(enemyMeshs[random.Next(0, 4)]);
+            et.speed = random.Next(enemySpeedMin, enemySpeedMax) / 10;
+            et.rotation = new Vector3(random.Next(1, 40) / 10.0f, random.Next(1, 40) / 10.0f, random.Next(1, 40) / 10.0f);
+
+            float etscale = enemyScaleMax - enemyScaleMax * 10 * et.speed / enemySpeedMax;
+            et.body.transform.localScale = new Vector3(etscale, etscale, etscale);
+            et.body.GetComponent<Rigidbody>().mass = etscale;
+
+            et.body.transform.position = new Vector3(random.Next(-10, 10), random.Next(-15, 15), spawner.transform.position.z);
+            et.body.transform.rotation = new Quaternion((float)random.NextDouble() * 1000 % 360, (float)random.NextDouble() * 1000 % 360, (float)random.NextDouble() * 1000 % 360, 1);
+
+            enemies.Add(et);
+        }
     }
 
     void FixedUpdate() {
-        if (enemies.Count < maxEnemies) {
-            enemyTemp = new Enemy ();
-            enemyTemp.prefab = Instantiate(prefab);
-
-            enemyTemp.speed = random.Next((int)minEnemySpeed, (int)maxEnemySpeed);
-
-            float eScale = maxEnemyScale / enemyTemp.speed;
-
-            enemyTemp.prefab.GetComponent<Rigidbody>().mass = eScale;
-            enemyTemp.prefab.transform.localScale = new Vector3(eScale * 100, eScale * 100, eScale * 100);
-
-            enemyTemp.prefab.transform.position = new Vector3(random.Next(-10, 10), random.Next(-15, 15), spawner.transform.position.z);
-
-            enemies.Add (enemyTemp);
-        }
         for (int i = 0; i < enemies.Count; i++) {
-            if (enemies[i].prefab.transform.position.z < -20 ||
-                Mathf.Abs(enemies[i].prefab.transform.position.x) > 70 ||
-                Mathf.Abs(enemies[i].prefab.transform.position.y) > 35) {
-                DestroyImmediate (enemies[i].prefab, true);
-                enemies.RemoveAt (i);
+            if (enemies[i].body.transform.position.z < -20 ||
+                Mathf.Abs(enemies[i].body.transform.position.x) > 70 ||
+                Mathf.Abs(enemies[i].body.transform.position.y) > 35) {
+                DestroyImmediate(enemies[i].body, true);
+                enemies.RemoveAt(i);
             }
             else {
-                enemies[i].prefab.transform.Translate(0, 0, (float)enemies[i].speed * 0.1f);
+                enemies[i].body.transform.Rotate(enemies[i].rotation);
+                enemies[i].body.transform.Translate(0, 0, -enemies[i].speed * 0.1f, Space.World);
             }
         }
     }
